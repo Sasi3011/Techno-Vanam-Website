fd8fc4ddeceeb239f2757bbc657d71f40164b34d
Sasi <sasikiran.tt3011@gmail.com>
2025-07-25 12:37:05 +0530

    Removed splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -10,51 +10,21 @@ import Product1 from "./pages/Products/Product1";
 import Product2 from "./pages/Products/Product2";
 
 import MainLayout from "./layout/MainLayout";
-import SplashScreen from './components/SplashScreen';
 
-function App() {
-  const [showSplash, setShowSplash] = useState(true);
-  const [fadeOut, setFadeOut] = useState(false);
-
-  useEffect(() => {
-    const isSlowConnection = navigator.connection &&
-      (navigator.connection.effectiveType === '2g' || navigator.connection.saveData);
-
-    const delay = isSlowConnection ? 5000 : 3000;
-
-    const fadeOutTimer = setTimeout(() => {
-      setFadeOut(true); // Start fading
-    }, delay);
-
-    const removeSplashTimer = setTimeout(() => {
-      setShowSplash(false); // Remove splash after fade completes
-    }, delay + 600); // Wait for fade to finish (600ms)
-
-    return () => {
-      clearTimeout(fadeOutTimer);
-      clearTimeout(removeSplashTimer);
-    };
-  }, []);
-
+const App = () => {
   return (
-    <>
-      {showSplash ? (
-        <SplashScreen fadeOut={fadeOut} />
-      ) : (
-        <MainLayout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            {/* <Route path="/portfolio" element={<Portfolio />} /> */}
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/product1" element={<Product1 />} />
-            <Route path="/product2" element={<Product2 />} />
-          </Routes>
-        </MainLayout>
-      )}
-    </>
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        {/* <Route path="/portfolio" element={<Portfolio />} /> */}
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product1" element={<Product1 />} />
+        <Route path="/product2" element={<Product2 />} />
+      </Routes>
+    </MainLayout>
   );
-}
+};
 
 export default App;
